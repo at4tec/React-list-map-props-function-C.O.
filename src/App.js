bddba51dd175fec14a347cafd06ff8c4281e3b05
at4tec <at4tec@gmail.com>
2022-01-28 18:40:46 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  //const names = ['Ahmed', 'Aly', 'Abdo'];
-const persons=[
- { id:1,
-  name:'Ahmed',
-  age:30,
-  skill:'react'},
- { id:2,
-    name:'Aly',
-    age:20,
-    skill:'js'},
-]
-
+  const names = ['Ahmed', 'Aly', 'Abdo'];
   return (
     <ul>
 
@@ -21,9 +10,9 @@ const persons=[
       <li>{names[1]}</li>
       <li>{names[2]}</li>*/}
 
-  {persons.map(
+  {names.map(
        (e) => (
-        <li>{e.name} has  {e.age} and knows {e.skill} </li>
+        <li>{e}</li>
             ))
   }
 
